685e87a90dbf18ab3a4346cd945e0b7a7bfdec4d
Angarkgurav <65789927+angark@users.noreply.github.com>
2025-10-03 11:42:46 +0530

    Update meRoute.js

--- Backend/routes/meRoute.js
+++ Backend/routes/meRoute.js
@@ -1,19 +1,1 @@
-const express = require("express");
-const router = express.Router();
 
-// /api/me -> returns current logged in user
-router.get("/", (req, res) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "Not authenticated" });
-  }
-
-  res.json({
-    id: req.user._id,
-    name: req.user.name,
-    email: req.user.email,
-    picture: req.user.picture,
-    token: req.user.token
-  });
-});
-
-module.exports = router;
