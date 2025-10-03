1e3bac98742ba82f0bcc38088693d892bd7f0498
Angarkgurav <65789927+angark@users.noreply.github.com>
2025-10-03 12:07:51 +0530

    Update googleStrategy.js

--- Backend/config/googleStrategy.js
+++ Backend/config/googleStrategy.js
@@ -10,7 +10,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://bike-bechoo-6.onrender.com/auth/google/callback",
+        callbackURL: "https://bike-bechoo-6.onrender.com/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         console.log(profile)
